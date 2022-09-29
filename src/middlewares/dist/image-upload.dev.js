9077db5db15aa2868cdc37221d022b1dd9be7b68
Mo Yasser <alalmy475@gmail.com>
2022-09-29 22:21:16 +0200

    increase image size upload to 5mb

--- src/middlewares/dist/image-upload.dev.js
+++ src/middlewares/dist/image-upload.dev.js
@@ -1,24 +1,26 @@
-const multer = require('multer');
+"use strict";
 
-const imageUpload = multer({
+var multer = require('multer');
+
+var imageUpload = multer({
   // Image size limit.
   limits: {
-    fileSize: 50000000, // 5MB
+    fileSize: 50000000 // 5MB
+
   },
   // Where to store the image.
   storage: multer.diskStorage({
-    destination: (req, file, cb) => {
+    destination: function destination(req, file, cb) {
       cb(null, 'uploads/images');
-    },
+    }
   }),
   // File type validation.
-  fileFilter: (req, file, cb) => {
+  fileFilter: function fileFilter(req, file, cb) {
     if (!file.originalname.match(/\.(jpg|png|jpeg)$/)) {
       return cb(new Error('Please upload an image'));
     }
 
     cb(null, true);
-  },
+  }
 });
-
 module.exports = imageUpload;
